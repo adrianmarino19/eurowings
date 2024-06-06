c77e1185715d616d4ea002ca722c983790df6e36
Nils Bischof <nilsbischof@web.de>
2024-06-06 18:19:27 +0200

    frontned

--- Pages/Predict Competitor's Prices.py
+++ Pages/Predict Competitor's Prices.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import random
+
 
 # URL of the logo image
 logo_url = "https://upload.wikimedia.org/wikipedia/commons/b/b4/Eurowings_Logo.svg"
@@ -8,6 +11,8 @@ st.image(logo_url, width=400)
 st.write("## Competitor Flight Fare Predictions")
 
 st.write("Provide flight details to get price predictions of Eurowings' competitors.")
+
+st.write("---")
 
 # Create four columns
 col1, col2, col3, col4 = st.columns(4)
@@ -27,3 +32,34 @@ with col4:
 
 # Button
 st.button("Let him cook!")
+
+import streamlit as st
+
+st.write("---")
+
+st.write("Predicted Flight Fares:")
+
+
+# Define the competitors and dummy fares
+competitors = [
+    "Ryanair",
+    "EasyJet",
+    "Norwegian Air Shuttle",
+    "Wizz Air",
+    "Vueling Airlines",
+    "Transavia",
+    "Air Europa",
+    "Germanwings",
+    "TUI Airways",
+    "Condor"
+]
+
+# Generate random realistic prices in the range of $100 to $170 for each competitor
+dummy_fares = [round(random.uniform(100, 170)) for _ in range(len(competitors))]
+
+# Create a DataFrame
+data = {'Competitor': competitors, 'Flight Price': dummy_fares}
+df = pd.DataFrame(data)
+
+# Display the table in Streamlit
+st.table(df)
--- Pages/Skyscanner API Dashboard.py
+++ Pages/Skyscanner API Dashboard.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 # URL of the logo image
 logo_url = "https://upload.wikimedia.org/wikipedia/commons/b/b4/Eurowings_Logo.svg"
@@ -7,4 +9,8 @@ st.image(logo_url, width=400)
 
 st.write("## Skyscanner API Dashboard")
 
+st.write("---")
 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
